25bc835858a9ba096e9ae5fcf4a77558926d8382
Erik <allareri@gmail.com>
2020-03-21 15:59:05 +0000

    Add defer file close that closes over Copy err, ex 5.18

--- src/ch5/webcrawl/fetch.go
+++ src/ch5/webcrawl/fetch.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
 func main() {
-	url := os.Args[1]
+	if len(os.Args) < 2 {
+		log.Fatalln("Please supply a url")
+	}
+	fetch(os.Args[1])
+}
+
+// Exercise 5.18
+// Fetch downloads the URL and returns the name and length of the local file
+func fetch(url string) (filename string, n int64, err error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("HTTP request failed %v\n", err)
-		os.Exit(1)
+		return "", 0, err
 	}
-	b, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
 	if err != nil {
-		fmt.Printf("Unable to read all content from response %v\n", err)
-		os.Exit(1)
+		return "", 0, err
 	}
-	fmt.Fprint(os.Stdout, string(b))
+	n, err = io.Copy(f, resp.Body)
+	defer func() {
+		closeErr := f.Close()
+		if err == nil {
+			err = closeErr
+		}
+	}()
+	// Close file, but prefer error from Copy, if any
+	// if closeErr := f.Close(); err == nil {
+	// 	err = closeErr
+	// }
+	return local, n, err
 }
